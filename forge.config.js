ec720068d157d62ebd4352d90416303bb2df99cc
Matias Carpintini <carpintinimatias@gmail.com>
2024-09-24 16:28:59 -0300

    signing the releases

--- forge.config.js
+++ forge.config.js
@@ -38,6 +38,14 @@ module.exports = {
       },
     },
   ],
+  osxSign: {},
+  // ...
+  osxNotarize: {
+    tool: "notarytool",
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  },
   plugins: [
     {
       name: "@electron-forge/plugin-auto-unpack-natives",
